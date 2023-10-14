3385c30a5a771e4e443dab4aac5a6965f7635981
Razorfang <jamiegetty1@gmail.com>
2023-10-15 00:40:08 +1300

    Remove unnecessary loop function.

--- cpp/src/Device.cpp
+++ cpp/src/Device.cpp
@@ -8,50 +8,57 @@ Device::Device(std::string name, UnicastCommunicator *ucomm, MulticastCommunicat
 	multicomm = mcomm;
 }
 
-void Device::iterate(void) {
+int Device::powerOn(void) {
 	std::string reply;
 	DiscoveryRequestMessage drq;
-	DiscoveryReplyMessage drm(deviceModel, deviceName);
-	TestStartMessage tst(0, 0);
 	TestStopMessage tsp;
+	TestStartMessage tst(0, 0);
 
-	switch(state) {
-		case UNDISCOVERED:
-			reply = multicomm->getBlocking();
-			std::cout << "Message received: " << reply << std::endl;
-			if (drq.updateFromString(reply))
-			{
-				std::cout << "Update received: " << drq.toString() << std::endl;
-				multicomm->sendMessage(drm);
-				std::cout << "Reply sent: " << drm.toString() << std::endl;
-				state = IDLE;
-			}
-			break;
-		case IDLE:
-			std::cout << "Waiting to start or stop a test..." << std::endl;
-			reply = unicomm->getBlocking();
-			std::cout << "Message received: " << reply << std::endl;
-			if (tst.updateFromString(reply)) {
-				std::cout << "Start received: " << tst.toString() << std::endl;
-			}
-			else if (tsp.updateFromString(reply)) {
-				std::cout << "Stop received: " << tsp.toString() << std::endl;
-			}
-			else {
-				std::cout << "Unknown message!" << std::endl;
-			}
-			break;
-		case TESTING:
-			break;
-		case EXITING:
-		default:
-			break;
-	}
-}
+	while (state != EXITING)
+	{
+		switch(state) {
+			case UNDISCOVERED:
+				reply = multicomm->getBlocking();
+				std::cout << "Message received: " << reply << std::endl;
+				if (drq.updateFromString(reply))
+				{
+					DiscoveryReplyMessage drm(deviceModel, deviceName);
+					std::cout << "Update received: " << drq.toString() << std::endl;
+					multicomm->sendMessage(drm);
+					std::cout << "Reply sent: " << drm.toString() << std::endl;
+					state = IDLE;
+				}
+				break;
+			case IDLE:
+				std::cout << "Waiting to start or stop a test..." << std::endl;
+				reply = unicomm->getBlocking();
+				std::cout << "Message received: " << reply << std::endl;
+				if (tst.updateFromString(reply)) {
+					TestResultMessage trm("STARTED", "");
+					std::cout << "Start received: " << reply << std::endl;
+					unicomm->sendMessage(trm);
+					std::cout << "Response sent: " << trm.toString() << std::endl;
+					state = TESTING;
+				}
+				else if (tsp.updateFromString(reply)) {
+					TestResultMessage trm("error", "ALREADY-STOPPED");
+					std::cout << "Stop received: " << reply << std::endl;
+					unicomm->sendMessage(trm);
+					std::cout << "Response sent: " << trm.toString() << std::endl;
+				}
+				else {
+					std::cout << "Unknown message!" << std::endl;
+				}
+				break;
+			case TESTING:
+				break;
+			case EXITING:
+			default:
+				break;
+		}
 
-int Device::powerOn() {
-	while (state != EXITING) {
-		iterate();
 	}
+
 	return 0;
 }
+
